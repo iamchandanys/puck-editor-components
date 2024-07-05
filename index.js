a0e33f854559e4ced14ce1a02ada738a41d3dddf
Chandan Y S <chandan@briisk.io>
2024-07-05 18:37:12 +0530

    added proj files

---
+++ index.js
@@ -0,0 +1,10 @@
+const InsuranceTypes = require("./output/components/InsuranceTypes").default;
+const FeaturesListCard =
+  require("./output/components/FeaturesListCard").default;
+const ViWithHeading = require("./output/components/ViWithHeading").default;
+
+module.exports = {
+  InsuranceTypes,
+  FeaturesListCard,
+  ViWithHeading,
+};
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+declare module "puck-editor-components" {
+  import { ComponentType } from "react";
+
+  export const InsuranceTypes: ComponentType;
+  export const FeaturesListCard: ComponentType;
+  export const ViWithHeading: ComponentType;
+}
